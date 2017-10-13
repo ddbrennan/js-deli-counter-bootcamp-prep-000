f03ecad95877e9d755e3c24671b9294f098dd7be
David Brennan <ddbren@gmail.com>
2017-10-13 23:02:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function nowServing(katzDeliLine) {
 
 function currentLine (katzDeliLine) {
   if (katzDeliLine.length > 0 ) {
-    var line = 'The line is currently: '
+    var line = `The line is currently: `
     for (let i = 0; i < katzDeliLine.length; i++) {
-      line = line + '${i+1}. ${katzDeliLine[i]}'
+      line = line + `${i+1}. ${katzDeliLine[i]}``
     }
     return line
   } else {
