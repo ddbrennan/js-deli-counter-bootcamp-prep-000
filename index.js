1291d420a476609d7482bb31846ceec941bf5cc9
David Brennan <ddbren@gmail.com>
2017-10-13 23:00:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,11 @@ function nowServing(katzDeliLine) {
 
 function currentLine (katzDeliLine) {
   if katzDeliLine.length > 0 {
-    for (let i = 0; i < katzDeliLine.length; )  
+    var line = 'The line is currently: '
+    for (let i = 0; i < katzDeliLine.length; i++) {
+      line = line + '${i+1}. ${katzDeliLine[i]}'
+    }
+    return line
   } else {
   return "The line is currently empty."
   }
