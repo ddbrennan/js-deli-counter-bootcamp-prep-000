d68a047757e23844a4d35b6de4532e7501b718f7
David Brennan <ddbren@gmail.com>
2017-10-13 22:46:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
-  var n = katzDeliLine.length
-  var greeting = `Welcome, ${name}. You are number ${n} in line.`
-  return greeting
+  return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
