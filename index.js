10e645c758042b2701dd4c91753fc57d185d9368
David Brennan <ddbren@gmail.com>
2017-10-13 22:48:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name)
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
+
+function nowServing() {
+  if (katzDeliLine.length > 0) {
+    return `Now serving katzDeliLine[0]`
+  } else {
+    return "There is nobody waiting to be served!"
+  }
+}
